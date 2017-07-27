0db7dfdd9b2f7fe168e1f9109527ea285f15e299
wolviet <g.tunnicliff@gmail.com>
2017-07-27 11:54:21 +0200

    modified mac 27-7-17

---
+++ test-MyMacVS/test-MyMacVS/TestController.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using myMacVS.Controllers;
+
+namespace test_MyMacVS
+{
+    public class TestController
+    {
+        [Fact]
+        public void TestAbout()
+        {
+			var controller = new HomeController();
+			var result = controller.About() as ViewResult;
+
+			Assert.Equal("Your application description page.", result.ViewData["Message"]);
+        }
+    }
+}
